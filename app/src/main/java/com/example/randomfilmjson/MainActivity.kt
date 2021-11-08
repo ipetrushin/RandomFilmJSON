acd8a5a1c6c1b966517bf66ed891575bb912b250
Ivan Petr <ivan.kiel@gmail.com>
2021-11-08 12:32:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/randomfilmjson/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.randomfilmjson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.Gson
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gson = Gson()
+    }
+}
